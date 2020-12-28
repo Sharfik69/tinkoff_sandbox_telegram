22f30294296f2ff38bca1527aabd5e391b328232
Dmitry Zaguzin <cindersstone@gmail.com>
2020-12-29 01:05:01 +0800

    Реализовал интерфейс покупки акций, по тикеру

--- main.py
+++ main.py
@@ -80,9 +80,90 @@ def get_balance(message):
     bot.send_message(message.from_user.id, msg)
 
 
+@bot.message_handler(commands=['stocks_list'])
+def get_stocks(message):
+    #TODO: Ченить сделать
+    aa = Account.get_list_stocks()
+    bot.send_message(message.from_user.id, str(aa))
+
+
+@bot.message_handler(commands=['search_by_ticker'])
+def search_by_ticker(message):
+    bot.send_message(message.from_user.id, 'Введите тикер ценной бумаги')
+    bot.register_next_step_handler(message, ticker_search)
+
+def ticker_search(message):
+    ticker = message.text
+    response = Account.search_by_ticker(ticker)
+    bot.send_message(message.from_user.id, response)
+
+
+@bot.message_handler(commands=['buy_by_ticker'])
+def buy_ticker(message):
+    bot.send_message(message.from_user.id, 'Введите тикер ценной бумаги')
+    bot.register_next_step_handler(message, show_price_list)
+
+
+def show_price_list(message):
+    ans = Account.show_price_list(message.text)
+    if not ans:
+        bot.send_message(message.from_user.id, 'По такому тикеру ничего не удалось найти')
+    else:
+        price_list = ''
+        markup = types.ReplyKeyboardMarkup(row_width=1, one_time_keyboard=True)
+
+        for price, cnt in ans[0]:
+            pr = '{0} {1}'.format(price, ans[1])
+            price_list += pr + '\n'
+            markup.add(types.KeyboardButton(pr))
+        price_list += 'Введите свою цену покупки, или укажите рыночную'
+        markup.add(types.KeyboardButton('Отмена покупки'))
+        bot.send_message(message.from_user.id, price_list, reply_markup=markup)
+        bot.register_next_step_handler(message, set_price)
+
+
+def set_price(message):
+    if message.text == 'Отмена покупки':
+        bot.send_message(message.from_user.id, 'Процесс покупки прерван')
+        return
+    global stock_price, stock_cnt
+    markup = types.ReplyKeyboardMarkup(row_width=1, one_time_keyboard=True)
+    markup.add(types.KeyboardButton('Отмена'))
+
+    try:
+        stock_price = Decimal(message.text.split(' ')[0])
+    except Exception:
+        bot.send_message(message.from_user.id, 'Процесс покупки прерван, произошла ошибка')
+        return
+
+    bot.send_message(message.from_user.id, 'Введите количество лотов', reply_markup=markup)
+    bot.register_next_step_handler(message, set_lots_and_buy)
+
+
+def set_lots_and_buy(message):
+    if message == 'Отмена':
+        bot.send_message(message.from_user.id, 'Процесс покупки прерван')
+        return
+
+    markup = types.ReplyKeyboardRemove(selective=False)
+    global stock_price, stock_cnt
+    try:
+        stock_cnt = int(message.text)
+    except Exception:
+        bot.send_message(message.from_user.id, 'Процесс покупки прерван', reply_markup=markup)
+        return
+
+    status = Account.buy_by_ticker(stock_price, stock_cnt)
+    if status:
+        bot.send_message(message.from_user.id, 'Заявка выставлена', reply_markup=markup)
+    else:
+        bot.send_message(message.from_user.id, 'Недостаточно средств', reply_markup=markup)
+
+
+
 @bot.message_handler(commands=['test'])
 def test(message):
-    pass
+    Account.test()
 
     # a = SandboxSetCurrencyBalanceRequest(balance=Decimal('100.0'), currency=SandboxCurrency.rub)
     # client.set_sandbox_currencies_balance(a)
--- user.py
+++ user.py
@@ -1,7 +1,8 @@
 from decimal import Decimal
-
+import random
 import tinvest
-from tinvest import SandboxRegisterRequest, BrokerAccountType, SandboxSetCurrencyBalanceRequest, SandboxCurrency
+from tinvest import SandboxRegisterRequest, BrokerAccountType, SandboxSetCurrencyBalanceRequest, SandboxCurrency, \
+    MarketOrderRequest, OperationType, LimitOrderRequest
 
 import settings
 
@@ -24,11 +25,67 @@ class User:
     def top_up_balance(self, balance, curr):
         current_balance = self.get_balance(beatiful=False)
         already_have = current_balance[curr]
-        a = SandboxSetCurrencyBalanceRequest(balance=Decimal(balance) + already_have, currency=self.our_currency.get(curr, SandboxCurrency.rub))
+        a = SandboxSetCurrencyBalanceRequest(balance=Decimal(balance) + already_have,
+                                             currency=self.our_currency.get(curr, SandboxCurrency.rub))
         self.client.set_sandbox_currencies_balance(a)
         return True
 
     def get_balance(self, beatiful=True):
         portfolio = self.client.get_portfolio_currencies()
-        sum_dict = {self.messages_properties.get(x.currency.name, x.currency.name) if beatiful else x.currency.name: x.balance for x in portfolio.payload.currencies}
+        sum_dict = {
+            self.messages_properties.get(x.currency.name, x.currency.name) if beatiful else x.currency.name: x.balance
+            for x in portfolio.payload.currencies}
         return sum_dict
+
+    def get_list_stocks(self):
+        a = self.client.get_market_stocks()
+        x = a.payload.instruments[0]
+        l = [(i.name, i.ticker) for i in a.payload.instruments]
+        random.shuffle(l)
+        return l[:10]
+
+    def search_by_ticker(self, ticker='AAPL'):
+        a = self.client.get_market_search_by_ticker(ticker=ticker)
+        ans = ''
+        for i in a.payload.instruments:
+            ans += '{0}: {1}\n'.format(i.ticker, i.name)
+        return 'Ничего не найдено' if ans == '' else ans
+
+    def show_price_list(self, ticker):
+        a = self.client.get_market_search_by_ticker(ticker=ticker)
+        if len(a.payload.instruments) == 1:
+            self.figi = a.payload.instruments[0].figi
+            print('figi = ' + self.figi)
+            market = self.client.get_market_orderbook(figi=self.figi, depth=5)
+            data = []
+            for i in market.payload.asks:
+                data.append([i.price, i.quantity])
+
+            return data, self.messages_properties[a.payload.instruments[0].currency.name]
+        else:
+            return False
+
+    def buy_by_ticker(self, price, cnt):
+        try:
+            aa = LimitOrderRequest(lots=cnt, operation=OperationType.buy, price=price)
+            self.client.post_orders_limit_order(figi=self.figi, body=aa)
+            return True
+        except Exception:
+            return False
+
+    def test(self):
+        # Поиск акций по тикеру
+        a = self.client.get_market_search_by_ticker(ticker='AAPL')
+        # print(a)
+
+        # Список всех акций
+        a = self.client.get_market_stocks()
+        print(len(a.payload.instruments))
+        # Список всех облигаций
+        a = self.client.get_market_bonds()
+
+        # Список фондов
+        a = self.client.get_market_etfs()
+
+        # Вывод заявок на продажу
+        a = self.client.get_market_orderbook(figi='BBG000B9XRY4', depth=10)
