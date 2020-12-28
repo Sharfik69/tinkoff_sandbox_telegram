5234139790b93eecd826215cb4de0d8bb3a643d9
Dmitry Zaguzin <cindersstone@gmail.com>
2020-12-28 21:42:33 +0800

    Добавил возможность пополнять счет

--- main.py
+++ main.py
@@ -1,12 +1,23 @@
+import json
+from decimal import Decimal
+import re
 import telebot
+from telebot import types
+from tinvest import SandboxRegisterRequest, SandboxSetCurrencyBalanceRequest, SandboxCurrency, BrokerAccountType, \
+    UserAccount
+
 import settings
 import database_work
+import tinvest
+
+from user import User
 
 bot = telebot.TeleBot(settings.__TELEGRAM_TOKEN__, parse_mode=None)
 db = database_work.DbWorker()
 
 auth_users = db.get_true_users()
 
+Account = User()
 
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
@@ -30,6 +41,49 @@ def auth_user(message):
         bot.send_message(message.from_user.id, 'Доступ разрешен')
     else:
         bot.send_message(message.from_user.id, 'Неверный ключ')
+
+
+@bot.message_handler(commands=['top_up_balance'])
+def top_up_balance(message):
+    global auth_users
+    if message.from_user.id not in auth_users:
+        bot.send_message(message.from_user.id, 'У вас нет доступа')
+        return
+    else:
+        markup = types.ReplyKeyboardMarkup(row_width=3)
+        itembtn1 = types.KeyboardButton('70000 rub')
+        itembtn2 = types.KeyboardButton('800 eur')
+        itembtn3 = types.KeyboardButton('1000 usd')
+        markup.add(itembtn1, itembtn2, itembtn3)
+        bot.send_message(message.from_user.id, 'Выберите сумму начисления, или введите ее сами', reply_markup=markup)
+        bot.register_next_step_handler(message, set_sum)
+
+
+def set_sum(message):
+    markup = types.ReplyKeyboardRemove(selective=False)
+    if re.match(r"[0-9]{1,} [a-zA-Z]{3}", message.text):
+        sum, curr = message.text.split(' ')
+        Account.top_up_balance(sum, curr)
+        bot.send_message(message.from_user.id, 'Начислено ✅', reply_markup=markup)
+    else:
+        bot.send_message(message.from_user.id, 'Ошибка ⛔️', reply_markup=markup)
+
+
+@bot.message_handler(commands=['balance'])
+def get_balance(message):
+    d = Account.get_balance()
+    msg = 'На нашем счету:\n'
+    for curr, sum in d.items():
+        msg += '{0} {1}\n'.format(sum, curr)
+    bot.send_message(message.from_user.id, msg)
+
+
+@bot.message_handler(commands=['test'])
+def test(message):
+    pass
+
+    # a = SandboxSetCurrencyBalanceRequest(balance=Decimal('100.0'), currency=SandboxCurrency.rub)
+    # client.set_sandbox_currencies_balance(a)
 
 
 bot.polling()
--- user.py
+++ user.py
@@ -1,3 +1,35 @@
+from decimal import Decimal
+
+import tinvest
+from tinvest import SandboxRegisterRequest, BrokerAccountType, SandboxSetCurrencyBalanceRequest, SandboxCurrency
+
+import settings
+
+
 class User:
     def __init__(self):
-        pass
+        self.client = tinvest.SyncClient(settings.__TINKOFF_API__, use_sandbox=True)
+
+        self.client.clear_sandbox_account()
+
+        xx = SandboxRegisterRequest(broker_account_type=BrokerAccountType.tinkoff)
+        self.client.register_sandbox_account(xx)
+
+        self.our_currency = {'rub': SandboxCurrency.rub, 'eur': SandboxCurrency.eur, 'usd': SandboxCurrency.usd}
+
+        self.messages_properties = {'usd': 'Долларов', 'rub': 'Рублей', 'eur': 'Евро'}
+
+        print('Создали новый акк')
+
+    def top_up_balance(self, balance, curr):
+        current_balance = self.get_balance(beatiful=False)
+        already_have = current_balance[curr]
+        a = SandboxSetCurrencyBalanceRequest(balance=Decimal(balance) + already_have, currency=self.our_currency.get(curr, SandboxCurrency.rub))
+        self.client.set_sandbox_currencies_balance(a)
+        print(balance, curr)
+        return True
+
+    def get_balance(self, beatiful=True):
+        portfolio = self.client.get_portfolio_currencies()
+        sum_dict = {self.messages_properties.get(x.currency.name, x.currency.name) if beatiful else x.currency.name: x.balance for x in portfolio.payload.currencies}
+        return sum_dict
